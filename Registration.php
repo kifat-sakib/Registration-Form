3406f2a86e6d8712c6882380469b2e4f0fd17dbb
Kifat Sakib <kifatsakib77@gmail.com>
2021-07-01 13:31:50 +0600

    Registration.php

---
+++ Registration.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="description" content="This Is a page practice about HTML 5 inputs & forms">
+<link rel="stylesheet" type="text/css" href="styles.css">
+<title>Registration</title>
+</head>
+<body>
+ <header>
+
+ </header>
+ <main>
+  <div class="loginBox">
+   <h2>Registration</h2>
+   
+  <form>
+   <p>First Name</p>
+   	<input type="text" name="fname" placeholder="Firstname">
+   <p>Last Name</p>
+   	<input type="text" name="lname" placeholder="Lastname">
+	<p>Doctor's ID</p>
+	<input type="numberonly" id="doctor's id" placeholder="user id">
+   <p>Date Of Birth</p>
+   	<input type="date">
+   <p>E-mail Address</p>
+   	<input type="email" name="email" placeholder="e-mail">
+   <p>Password<p>
+	<input type="Password" name="pword1" placeholder="Create Password">
+   <p>Re-enter Password</p>
+	<input type="password" name="pword2" Placeholder="Confirm Password">
+   	<input type="Submit" name="sbmt">
+  
+  <p>Already have an account? <a href="#">Sign In</a></p>
+  </form>
+  </div>
+ </main>
+</body>
+</html>
